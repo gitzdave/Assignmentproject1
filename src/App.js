4f2dcc67989916d738e4b94cdd2549a74da96044
gitzdave <gitzdave@yahoo.com>
2021-11-13 19:27:15 +0000

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,43 @@ import React from "react";
 import "./style.css";
 import MedicalTreatment from "./MedicalTreatment";
 import MedicalTreatmentPanel from "./MedicalTreatmentPanel";
+import './App.css';
 
-var treatID = 1233
-var treatCourseId=12233
-var type= "Tretament"
+var treatId = "1233"
+var treatCourseId = "12233"
+var type = "Tretament"
 var category= "Vaccination"
 var name= "Mary"
 var startDate= "02/12/2021"
 
 
-    
-  
   
 
 export default function App() {
   return (
-    <div>
-
+    
+<div className="app-container">
+        <table>
+          <thead>
+            <tr>
+            <th>TreatID</th>
+              <th>Name</th>
+              <th>TreatCourseID</th>
+              <th>StartDate</th>
+              <th>category</th>
+              <th>Type</th>
+            </tr>  
+          </thead>
+          <tbody>
+            <td>{treatId} </td>
+           <td>{name} </td>
+           <td>{treatCourseId} </td>
+           <td>{startDate} </td>
+           <td>{category} </td>
+           <td>{type} </td>
+            </tbody>
+            </table>
+            
 <br/><br/>
 <MedicalTreatmentPanel />
     </div>
--- src/MedicalTreatmentPanel.js
+++ src/MedicalTreatmentPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MedicalTreatment from './MedicalTreatment';
+import nanoid from './nanoid';
 
 function MedicalTreatmentPanel(props){
     {props.treatId}
@@ -15,21 +16,46 @@ function MedicalTreatmentPanel(props){
     const [treatCourseId, settreatCourseId] = React.useState('');
     const [startDate, setstartDate] = React.useState('');
     const [type, settype] = React.useState('');
+    const [data,setData] = React.useState({
+        treatId:'',
+        treatCourseId:'',
+        type:'',
+        category:'',
+        name:'',
+        startDate:''}
 
-    function showSubmit (events) {
+    )
+    function display () {
         alert("[" + "name = "+ name+ ", treatID = "+ treatId + ", startDate = "+startDate +" ,type = " + type + " ,category= " + category + " ,treatCourseId =" + treatCourseId + "]" );
+    } 
+     function showSubmit (events) {
+     events.preventDefault();
     
-    events.preventDefault();
-    settreatId('');
-    setname('');
-    setcategory('');
-    settreatCourseId ("");
-    setstartDate ("");
-    settype ("");
-    
-
+    const fieldName = events.target.getAttribute('name')
+    const fieldValue = events.target.value;
+    const newForm = {...data};
+     newForm[fieldName]=fieldValue;
+     setData(newForm);
 
     }
+
+    function formSubmit (events) {
+        events.preventDefault();
+        
+        const record = {
+        treatId: data.treatId,
+        treatCourseId:data.treatCourseId,
+        type:data.type,
+        category:data.category,
+        name:data.name,
+        startDate: data.startDate
+    };
+    const newRecords =[...record,record]
+    setData(newRecords);
+    }
+
+
+
     function clearClickHandler () {
         settreatId('');
         setname('');
@@ -41,35 +67,32 @@ function MedicalTreatmentPanel(props){
 
 
     return(
-        <form onSubmit = {showSubmit}>
+        <form >
             <h1>  Medical Records  </h1>
 
-            <input placeholder="TreatID" value={treatId}  onChange = { e => settreatId(e.target.value)}  ></input>
+            <input placeholder="TreatID" type="text"  onChange = { showSubmit} name= "treatId" ></input>
 
             <br/>
 
-            <input placeholder=" Name" value={name}  onChange = { e => setname(e.target.value)}  ></input>
+            <input placeholder=" Name" type="text"  onChange={showSubmit} name="name" ></input>
 
             <br/>
 
-            <input placeholder="StartDate" value={startDate}  onChange = { e => setstartDate(e.target.value)}  ></input>
+            <input placeholder="StartDate" type="text"  onChange = { showSubmit} name="startDate"  ></input>
             
             <br/>
 
-            <input placeholder="TreatCourseID " value={treatCourseId}  onChange = { e => settreatCourseId(e.target.value)}  ></input>
+            <input placeholder="TreatCourseID "type="text"    onChange = { showSubmit} name="treatCourseId" ></input>
             <br/>
 
-            <input placeholder="Category" value={category}  onChange = { e => setcategory(e.target.value)}  ></input>
+            <input placeholder="Category"type="text"   onChange = {showSubmit}name ="category"  ></input>
             <br/>
-            <input placeholder="Type" value={type}  onChange = { e => settype(e.target.value)}  ></input>
+            <input placeholder="Type" type="text"   onChange = { showSubmit} name="type"  ></input>
 
-            <br/><br/>
-         <hr/>
-
-         {name  && treatCourseId   && category && type && startDate && treatId &&
-          <input  type = "submit" value=" Show treatment Records "  display = "in-line-block" />}
-
+          <button type = "submit">Add </button>
+          <button onClick = {clearClickHandler}> Clear</button>
         </form>
+        
     );
 }
 

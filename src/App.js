3cdd8ba8ac21323e9074afa5d9f2e89997bf6856
gitzdave <gitzdave@yahoo.com>
2021-12-03 11:34:37 +0000

    2 files modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "./style.css";
-import MedicalTreatment from "./MedicalTreatment";
 import MedicalTreatmentPanel from "./MedicalTreatmentPanel";
 import './App.css';
-import MedicalTreatmentList from "./MedicalTreatmentList";
 
 
-
-
-  
+ 
 
 export default function App() {
   return (
--- src/MedicalTreatmentPanel.js
+++ src/MedicalTreatmentPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import MedicalTreatment from './MedicalTreatment';
-import nanoid from './nanoid';
+
+
 
 function MedicalTreatmentPanel(props){
     
@@ -10,7 +10,7 @@ function MedicalTreatmentPanel(props){
     const [name, setName] = React.useState('');
     const [category, setCategory] = React.useState('');
     const [treatCourseId, setTreatCourseId] = React.useState('');
-    const [startDate, startDate] = React.useState('');
+    const [startDate, setStartDate] = React.useState('');
     const [type, setType] = React.useState('');
 
 
@@ -37,11 +37,11 @@ function MedicalTreatmentPanel(props){
         <form >
             <h1>  Medical Records  </h1>
 
-            <input placeholder="TreatID" value = {treatId} type="text"  onChange = {e=> settreatId (e.target.value)} name= "treatId" ></input>
+            <input placeholder="TreatID" value = {treatId} type="text"  onChange = {e=> setTreatId (e.target.value)} name= "treatId" ></input>
 
             <br/>
 
-            <input placeholder=" Name" value= {name} type="text"  onChange={e=> setname (e.target.value)} name="name" ></input>
+            <input placeholder=" Name" value= {name} type="text"  onChange={e=> setName (e.target.value)} name="name" ></input>
 
             <br/>
 
